a53f81a5519fa8f132377333145452179a2a2c84
Anil Pawar <124265148+Anil1110@users.noreply.github.com>
2023-02-02 18:16:45 +0530

    Add files via upload

---
+++ selection.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  3 15:46:44 2020
+
+@author: USER
+"""
+
+
+ind=0
+def db(a):
+    if a=='2010':
+        ind=0
+        return(ind)
+    elif a=='2011':
+        ind=1
+        return(ind)
+    elif a=='2012':
+        ind=2
+        return(ind)
+    elif a=='2013':
+        ind=3
+        return(ind)
+    elif a=='2014':
+        ind=4
+        return(ind)
+    elif a=='2015':
+        ind=5
+        return(ind)
+    elif a=='2016':
+        ind=6
+        return(ind)
+    elif a=='2017':
+        ind=7
+        return(ind)
+    elif a=='2018':
+        ind=8
+        return(ind)
+    elif a=='2009':
+        ind=9
+        return(ind)
+    elif a=='2008':
+        ind=10
+        return(ind)
+    
+    
